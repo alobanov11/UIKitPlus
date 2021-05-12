a21631aa8dec01f2fd69120e475d1cfda3bb21f2
Anton Lobanov <alobanov11@gmail.com>
2021-05-12 20:59:07 +0300

    Improve

--- Classes/Controllers/BaseApp/_SceneDelegate.swift
+++ Classes/Controllers/BaseApp/_SceneDelegate.swift
@@ -36,36 +36,37 @@ class _SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-        BaseApp.shared.lifecycle?._didBecomeActive?()
+        UIApplication.shared.delegate?.applicationDidBecomeActive?(UIApplication.shared)
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
-        BaseApp.shared.lifecycle?._willResignActive?()
+        UIApplication.shared.delegate?.applicationWillResignActive?(UIApplication.shared)
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
-        BaseApp.shared.lifecycle?._willEnterForeground?()
+        UIApplication.shared.delegate?.applicationWillEnterForeground?(UIApplication.shared)
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-        BaseApp.shared.lifecycle?._didEnterBackground?()
+        UIApplication.shared.delegate?.applicationDidEnterBackground?(UIApplication.shared)
     }
 
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         guard let url = URLContexts.first?.url else {
             return
         }
-        let _ = BaseApp.shared.lifecycle?._openURLSourceAppAnnotation?(
-            url,
-            nil,
-            [UIApplication.OpenURLOptionsKey.annotation]
+        UIApplication.shared.delegate?.application?(
+            UIApplication.shared,
+            open: url,
+            sourceApplication: nil,
+            annotation: [UIApplication.OpenURLOptionsKey.annotation]
         )
     }
     

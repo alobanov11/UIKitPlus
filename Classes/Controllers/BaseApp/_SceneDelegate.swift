f3ef154c022038b8f8cce83c86226d872ced7a71
Anton Lobanov <alobanov11@gmail.com>
2021-05-12 20:53:28 +0300

    Improve

--- Classes/Controllers/BaseApp/_SceneDelegate.swift
+++ Classes/Controllers/BaseApp/_SceneDelegate.swift
@@ -36,22 +36,26 @@ class _SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        BaseApp.shared.lifecycle?._didBecomeActive?()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        BaseApp.shared.lifecycle?._willResignActive?()
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        BaseApp.shared.lifecycle?._willEnterForeground?()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        BaseApp.shared.lifecycle?._didEnterBackground?()
     }
 
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {

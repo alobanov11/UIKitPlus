9097b395d6098fe56961ecc505d78c524b88bf22
Anton Lobanov <alobanov11@gmail.com>
2021-05-31 19:25:01 +0300

    Refactor UCollection

--- Classes/Collection/CollectionBuilder.swift
+++ Classes/Collection/CollectionBuilder.swift
@@ -6,13 +6,16 @@
 public enum CollectionBuilder<Element> {
     public typealias Expression = Element
     public typealias Component = [Element]
-    
-    public static func buildExpression(_ expression: Expression) -> Component { [expression] }
-    public static func buildExpression(_ expression: Expression?) -> Component { expression.map { [$0] } ?? [] }
+
     public static func buildBlock(_ children: Component...) -> Component { children.flatMap { $0 } }
-    public static func buildOptional(_ children: Component?) -> Component { children ?? [] }
     public static func buildBlock(_ component: Component) -> Component { component }
-    public static func buildIf(_ expression: Expression?) -> Component { expression.map { [$0] } ?? [] }
-    public static func buildEither(first child: Component) -> Component { child }
-    public static func buildEither(second child: Component) -> Component { child }
+
+    public static func buildExpression(_ expression: Expression) -> Component { [expression] }
+    public static func buildExpression(_ component: Component) -> Component { component }
+
+    public static func buildEither(first component: Component) -> Component { component }
+    public static func buildEither(second component: Component) -> Component { component }
+
+    public static func buildEither(first expression: Expression) -> Component { [expression] }
+    public static func buildEither(second expression: Expression) -> Component { [expression] }
 }

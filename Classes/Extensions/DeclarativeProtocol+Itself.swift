a00f4237b1f8113c4e09617426a06f1b9b3be3b6
Anton Lobanov <alobanov11@gmail.com>
2021-09-08 11:11:16 +0300

    Fix itself

--- Classes/Extensions/DeclarativeProtocol+Itself.swift
+++ Classes/Extensions/DeclarativeProtocol+Itself.swift
@@ -1,9 +1,11 @@
 extension DeclarativeProtocol {
+	@discardableResult
     public func itself(_ itself: inout Self?) -> Self {
         itself = self
         return self
     }
 
+	@discardableResult
     public func configure(_ closure: (Self) -> Void) -> Self {
         closure(self)
         return self

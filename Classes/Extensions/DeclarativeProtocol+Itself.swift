85e400bbb01f050711ac6d3ee133dee099d7c1a4
Anton Lobanov <alobanov11@gmail.com>
2021-10-04 20:44:30 +0300

    Add helpers

--- Classes/Extensions/DeclarativeProtocol+Itself.swift
+++ Classes/Extensions/DeclarativeProtocol+Itself.swift
@@ -10,4 +10,21 @@ extension DeclarativeProtocol {
         closure(self)
         return self
     }
+
+	@discardableResult
+	func bind<T>(_ state: State<T>, _ objectKeyPath: ReferenceWritableKeyPath<Self, T>) -> Self {
+		state.listen { [weak self] in
+			self?[keyPath: objectKeyPath] = $0
+		}
+		return self
+	}
+
+	@discardableResult
+	func bind<T>(_ state: State<T>, _ closure: @escaping (Self, T) -> Void) -> Self {
+		state.listen { [weak self] in
+			guard let self = self else { return }
+			closure(self, $0)
+		}
+		return self
+	}
 }

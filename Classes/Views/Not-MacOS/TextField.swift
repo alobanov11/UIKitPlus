16d82b79c7b8e77a2de18ea66b8c67ef91c1aec0
Anton Lobanov <alobanov11@gmail.com>
2021-10-21 20:48:25 +0300

    Fix secure

--- Classes/Views/Not-MacOS/TextField.swift
+++ Classes/Views/Not-MacOS/TextField.swift
@@ -376,7 +376,25 @@ extension UTextField: _TextAligmentable {
 
 extension UTextField: _Secureable {
     func _setSecure(_ v: Bool) {
-        isSecureTextEntry = v
+		isSecureTextEntry = v
+
+		if let existingText = text, isSecureTextEntry {
+			/* When toggling to secure text, all text will be purged if the user
+			 continues typing unless we intervene. This is prevented by first
+			 deleting the existing text and then recovering the original text. */
+			deleteBackward()
+
+			if let textRange = textRange(from: beginningOfDocument, to: endOfDocument) {
+				replace(textRange, withText: existingText)
+			}
+		}
+
+		/* Reset the selected text range since the cursor can end up in the wrong
+		 position after a toggle because the text might vary in width */
+		if let existingSelectedTextRange = selectedTextRange {
+			selectedTextRange = nil
+			selectedTextRange = existingSelectedTextRange
+		}
     }
 }
 

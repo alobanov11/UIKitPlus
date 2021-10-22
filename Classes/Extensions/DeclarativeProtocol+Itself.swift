fc23df784bb9df5c0071618a380ee8011605ec09
Anton Lobanov <alobanov11@gmail.com>
2021-10-22 14:28:45 +0300

    Bind immediately

--- Classes/Extensions/DeclarativeProtocol+Itself.swift
+++ Classes/Extensions/DeclarativeProtocol+Itself.swift
@@ -13,6 +13,7 @@ extension DeclarativeProtocol {
 
 	@discardableResult
 	public func bind<T>(_ state: State<T>, _ objectKeyPath: ReferenceWritableKeyPath<Self, T>) -> Self {
+		self[keyPath: objectKeyPath] = state.wrappedValue
 		state.listen { [weak self] in
 			self?[keyPath: objectKeyPath] = $0
 		}
@@ -21,6 +22,7 @@ extension DeclarativeProtocol {
 
 	@discardableResult
 	public func bind<T>(_ state: State<T>, _ closure: @escaping (Self, T) -> Void) -> Self {
+		closure(self, state.wrappedValue)
 		state.listen { [weak self] in
 			guard let self = self else { return }
 			closure(self, $0)

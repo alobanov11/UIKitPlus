1ba3e3b446c2342818eca5765e5ffb84288239df
Anton Lobanov <alobanov11@gmail.com>
2022-01-29 17:38:41 +0300

    Add back title

--- Classes/Controllers/NavigationController.swift
+++ Classes/Controllers/NavigationController.swift
@@ -174,8 +174,10 @@ open class NavigationController<T: UIViewController>: UINavigationController, UI
     //    }
 
 	open override func pushViewController(_ viewController: UIViewController, animated: Bool) {
-		if let backTitle = self.backTitle {
-			viewController.navigationItem.backBarButtonItem = UBarButtonItem(backTitle)
+		DispatchQueue.main.async {
+			if let backTitle = self.backTitle {
+				viewController.navigationItem.backBarButtonItem = UBarButtonItem(backTitle)
+			}
 		}
 		super.pushViewController(viewController, animated: animated)
 	}

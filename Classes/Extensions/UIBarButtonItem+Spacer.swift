74e7504189c95758a4331f023fb62f2c72e39fdf
Anton Lobanov <alobanov11@gmail.com>
2022-05-18 20:41:44 +0300

    Add uibarbutton helper

---
+++ Classes/Extensions/UIBarButtonItem+Spacer.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Антон Лобанов on 18.05.2022.
+//
+
+import UIKit
+
+extension UIBarButtonItem {
+	public static func spacer(_ width: CGFloat = 20) -> UIBarButtonItem {
+		let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+		spacer.width = 20
+		return spacer
+	}
+}
